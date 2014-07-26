2ed63eb508f3a566b9b12dc250b6d373a4d45537
Rachel <rshorey@gmail.com>
2014-07-25 23:40:57 -0400

    added create and insert statements to get up and running on sqlfiddle.com

--- create_table_statements.sql
+++ create_table_statements.sql
@@ -1,11 +1,20 @@
-create table voting_rights_sample.sections
+#statements to create and load data in mysql.
+#you'll have to do a find-and-replace for "/path/to/file"
+#to whatever the actual path to your data files is
+#mysql doesn't allow me to use a variable there, sorry
+
+create schema voting_rights;
+
+
+create table voting_rights.sections
 (id int,
 section varchar(5),
 description varchar(140),
 status varchar(140));
 
 
-create table voting_rights_sample.states
+
+create table voting_rights.states
 (id int,
 name varchar(25),
 population bigint,
@@ -18,13 +27,38 @@ pct_hpi decimal(4,2),
 pct_mixed decimal(4,2),
 region varchar(1));
 
-create table voting_rights_sample.cities
+
+create table voting_rights.cities
 (name varchar(25),
-2013pop bigint,
+population bigint,
 area_sq_miles decimal(8,2),
 state_id int);
 
-create table voting_rights_sample.sections_states
+create table voting_rights.sections_states
 (state_id int,
 section_id int);
 
+
+load data infile '/path/to/file/sections.csv'
+into table voting_rights.sections
+fields terminated by ','
+lines terminated by '\r'
+ignore 1 lines;
+
+load data infile '/path/to/file/states.csv'
+into table voting_rights.states
+fields terminated by ','
+lines terminated by '\r'
+ignore 1 lines;
+
+load data infile '/path/to/file/cities.csv'
+into table voting_rights.cities
+fields terminated by ','
+lines terminated by '\r'
+ignore 1 lines;
+
+load data infile '/path/to/file/sections_states.csv'
+into table voting_rights.sections_states
+fields terminated by ','
+lines terminated by '\r'
+ignore 1 lines;
